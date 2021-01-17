95f12334dd3bbff0de4d16e7698fdabd19f0999a
Or Assayag <orassayag@gmail.com>
2021-01-17 15:33:10 +0200

    removed test component

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,4 @@
 import App from './App/App';
 import Icons from './Icons/Icons';
 import Images from './Images/Images';
-import Test from './Test/Test';
-export { App, Icons, Images, Test };
+export { App, Icons, Images };
