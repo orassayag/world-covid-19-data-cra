2b59b550146f6d65c53adf836f485cb203a85c93
Or Assayag <orassayag@gmail.com>
2021-01-17 15:54:31 +0200

    fix imports order issue

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,9 @@ import * as serviceWorker from './serviceWorker';
 import settings from './settings/settings';
 import './assert/css/master.scss';
 import { EnvironmentMode } from './core/enums';
+import app from './initiate/initiate';
 const { ENVIRONMENT_MODE } = settings;
 const isDevelopment = process.env.NODE_ENV === 'development' || ENVIRONMENT_MODE === EnvironmentMode.DEVELOPMENT;
-import app from './initiate/initiate';
 ReactDOM.render(app, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
